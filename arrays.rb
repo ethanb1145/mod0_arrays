dbcde601e240be3049b67092e32be2f9f7a79ec6
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-06-21 16:05:43 -0600

    Call each method

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ strings = ["One", "Two", "Three", "Four"]
 integers = [1, 10, 20, 30]
 floats = [1.5, 3.5, 10.5, 20.5]
 boo = [true, false, true, false]
+
+#Will output "three"
+strings[2]
+
+#Will output 1
+integers[0]
+
+#Will output 20.5
+floats[3]
+
+#Will output false
+boo[1]
