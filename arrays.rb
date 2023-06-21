2ab1b199172be7aea5c2cea0e775126b4603e467
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-06-21 16:08:19 -0600

    Add puts

--- arrays.rb
+++ arrays.rb
@@ -4,13 +4,13 @@ floats = [1.5, 3.5, 10.5, 20.5]
 boo = [true, false, true, false]
 
 #Will output "three"
-strings[2]
+puts strings[2]
 
 #Will output 1
-integers[0]
+puts integers[0]
 
 #Will output 20.5
-floats[3]
+puts floats[3]
 
 #Will output false
-boo[1]
+puts boo[1]
